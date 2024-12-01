f9d03096941fbd95f7b12a0d97e2006f907ec8c8
Lailla Souza <laillavasouza@gmail.com>
2024-11-30 21:30:03 -0300

    Update Certificado_selo.py

--- src/views/Certificado_selo.py
+++ src/views/Certificado_selo.py
@@ -1,26 +1,62 @@
 from tkinter import *
+from tkinter import messagebox
 
+def exibir_popup():
+    messagebox.showinfo("Solicitação Recebida", "Sua solicitação foi recebida. Aguarde nosso feedback.")
 
 janela = Tk()
-janela.title("Certificado e Selo de Responsabilidade Ambiental")
+janela.title("Certificado e Selo Eco Gestor")
 janela.geometry("1000x600")
 janela.configure(bg="#EAF7EC")
+
+titulo_certificado = Label(
+    janela,
+    text="Certificado e Selo Eco Gestor",
+    font=("Arial", 16, "bold"),
+    bg="#EAF7EC",
+    fg="#2A5729",
+    pady=10
+)
+titulo_certificado.pack()
 
 certificado_texto = (
     "O Eco Gestor oferece um certificado e selo de responsabilidade ambiental para empresas que realizam o descarte de resíduos eletrônicos de forma "
     "confiável e responsável. Para ser elegível ao selo basta usar nossos serviços frequentemente há mais de um semestre e fazer descarte/coleta responsável "
     "do e-lixo, depois é só renovar o certificado anualmente para garantir responsabilidade com o meio-ambiente."
 )
-texto_certificado = Label(janela, text=certificado_texto, font=("Arial", 12), justify="left", padx=10, pady=10, bg="#EAF7EC", wraplength=750)
+texto_certificado = Label(
+    janela,
+    text=certificado_texto,
+    font=("Arial", 12),
+    justify="left",
+    padx=10,
+    pady=10,
+    bg="#EAF7EC",
+    wraplength=750
+)
 texto_certificado.pack(pady=10)
 
-botao_aplicar_certificado = Button(janela, text="Quero aplicar para o certificado", font=("Arial", 12), width=30, bg="#2A5729", fg="white")
+botao_aplicar_certificado = Button(
+    janela,
+    text="Quero aplicar para o certificado",
+    font=("Arial", 12),
+    width=30,
+    bg="#2A5729",
+    fg="white",
+    command=exibir_popup
+)
 botao_aplicar_certificado.pack(pady=10)
 
-botao_acessar_certificado = Button(janela, text="Acessar certificado", font=("Arial", 12), width=30, bg="#2A5729", fg="white")
+botao_acessar_certificado = Button(
+    janela,
+    text="Acessar certificado",
+    font=("Arial", 12),
+    width=30,
+    bg="#2A5729",
+    fg="white"
+)
 botao_acessar_certificado.pack(pady=10)
 
-# Rodapé
 footer_frame = Frame(janela, bg="#2A5729", height=120)
 footer_frame.pack(side="bottom", fill="x")
 
@@ -32,7 +68,7 @@ left_text = Label(
     bg="#2A5729",
     justify="left",
     padx=20,
-    wraplength=350,  
+    wraplength=350,
     anchor="w"
 )
 left_text.pack(side="left", anchor="w", padx=10)
@@ -45,7 +81,7 @@ right_text = Label(
     bg="#2A5729",
     justify="right",
     padx=20,
-    wraplength=350, 
+    wraplength=350,
     anchor="e"
 )
 right_text.pack(side="right", anchor="e", padx=10)
